da2bff3b7781e5e281d86b6c694a93c5130d4d74
Suman Parajuli <sumansupercell@gmail.com>
2024-08-06 20:07:23 +0545

    change localhost string to baseurl

--- redux/apis/cartApiSlice.ts
+++ redux/apis/cartApiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const cartsApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
   tagTypes: ["showCarts"],
   reducerPath: "cartsApi",
   endpoints: (builder) => ({
--- redux/apis/productsApiSlice.ts
+++ redux/apis/productsApiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const productsApi = createApi({
     tagTypes: ['Products'],
     reducerPath: 'productsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => '/api/products',
