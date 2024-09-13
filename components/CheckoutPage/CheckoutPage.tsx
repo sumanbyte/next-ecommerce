3a04a2e070720b56e85118a2bc80715aa18f8b23
Suman Parajuli <sumansupercell@gmail.com>
2024-09-13 19:32:26 +0545

    checkout page trying to add features

--- components/CheckoutPage/CheckoutPage.tsx
+++ components/CheckoutPage/CheckoutPage.tsx
@@ -5,6 +5,8 @@ import {
   useUpdateCartMutation,
 } from '@/redux/apis/cartApiSlice';
 import { MdDelete } from "react-icons/md";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+
 import { useSelector } from 'react-redux';
 
 const CheckoutPage: React.FC = () => {
@@ -29,9 +31,26 @@ const CheckoutPage: React.FC = () => {
   return (
     <div className='max-w-7xl m-auto'>
       <div className='my-10 px-2 md:px-4'>
+
+
+
         <h1 className='py-5 text-3xl'>Checkout <span className='text-primary-600'>Page</span></h1>
+        <div className='flex gap-2'>
+          <div className='flex gap-2 items-center'>
+            <div className='bg-primary-700 px-2 py-1 rounded-md hover:bg-primary-800 transition cursor-pointer duration-200'>
+              <FaPlus className=' text-xs text-white ' />
+            </div>
+            <p>Select All</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <select className='px-2 py-1' defaultValue={""}>
+              <option value="">Choose Option</option>
+              <option value="">Delete Items</option>
+            </select>
+          </div>
+        </div>
         <div className='hidden lg:block'>
-          <div className='bg-white grid grid-cols-[2fr_1fr_1fr_1fr_1fr] justify-items-center py-5 mb-5 '>
+          <div className='bg-white grid grid-cols-[2fr_1fr_1fr_1fr_1fr] justify-items-center py-5 mb-2'>
             <p>Product</p>
             <p>Price</p>
             <p>Quantity</p>
@@ -43,22 +62,16 @@ const CheckoutPage: React.FC = () => {
               <div className='grid grid-col-checkout w-full justify-items-center'>
                 <Image className='w-8 h-8 inline-block' src={cartItem.productId.image} width={50} height={30} alt={cartItem.productId.title} />
                 <p className='w-full text-left'>
-                  {cartItem.productId.title.length > 40 ? `${cartItem.productId.title.substring(0, 40)}...` : cartItem.productId.title}
+                  {cartItem.productId.title}
                 </p>
               </div>
               <div>
                 <p>$ {cartItem.productId.price}</p>
               </div>
               <div className="flex items-center gap-5">
-                <button
-                  onClick={() => handleDecreaseQuantity(cartItem.productId._id)}
-                  className="px-4 text-2xl rounded-sm"
-                >-</button>
+                <FaMinus color='black' onClick={() => handleIncreaseQuantity(cartItem.productId._id)} className='rounded-sm' />
                 <p className='text-lg'>{cartItem.quantity}</p>
-                <button
-                  onClick={() => handleIncreaseQuantity(cartItem.productId._id)}
-                  className="px-4 text-2xl rounded-sm"
-                >+</button>
+                <FaPlus color='black' onClick={() => handleIncreaseQuantity(cartItem.productId._id)} className=' rounded-sm' />
               </div>
               <div>
                 <p>$ {(cartItem.productId.price * cartItem.quantity).toFixed(2)}</p>
@@ -90,9 +103,9 @@ const CheckoutPage: React.FC = () => {
                   <p className='font-semibold'>${cartItem.productId.price}</p>
                 </div>
                 <div className='flex justify-center items-center gap-5'>
-                  <p className='text-xl'>-</p>
-                  <p className='text-xl'>{cartItem.quantity}</p>
-                  <p className='text-xl'>+</p>
+                  <FaMinus color='black' onClick={() => handleIncreaseQuantity(cartItem.productId._id)} className='rounded-sm' />
+                  <p className='text-lg'>{cartItem.quantity}</p>
+                  <FaPlus color='black' onClick={() => handleIncreaseQuantity(cartItem.productId._id)} className=' rounded-sm' />
                 </div>
               </div>
             })
