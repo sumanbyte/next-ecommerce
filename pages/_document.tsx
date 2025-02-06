d6bb6ae74c2d8926b471873e88f0e8db5fa21c02
Suman Parajuli <sumansupercell@gmail.com>
2025-02-06 22:01:24 +0545

    favicons added and pwa support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,52 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className='bg-[#E4E5E4]'>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Standard Favicon */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+
+        {/* Apple Touch Icon (for iOS) */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Android Chrome Icons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body className="bg-[#E4E5E4]">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
